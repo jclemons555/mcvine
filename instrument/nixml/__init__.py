13e056e07a48aa50b43b9a22f373288391c53513
Jiao Lin <jiao.lin@gmail.com>
2020-09-20 23:04:10 -0400

    print_function

--- instrument/nixml/__init__.py
+++ instrument/nixml/__init__.py
@@ -11,6 +11,7 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 
+from __future__ import print_function
 
 from .Parser import Parser
 default_parser = Parser()
